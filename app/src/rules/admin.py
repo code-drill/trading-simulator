1a7e4c34333c4cb47dffc751a292e6fd5c8616bd
CodeDrillEu <code.drill.eu@gmail.com>
2025-05-23 17:09:54 +0200

    Adds Market model and admin configuration
    Adds Market model with code as primary key.
    Registers Market model in admin panel and unregisters default User and Group models.

--- app/src/rules/admin.py
+++ app/src/rules/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
+
+from rules.models import Market
 
 # Register your models here.
+
+admin.site.register(Market)
+admin.site.unregister(User)
+admin.site.unregister(Group)
--- app/src/rules/models.py
+++ app/src/rules/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Market(models.Model):
+    code = models.CharField(max_length=20, unique=True, primary_key=True)
+
+    def __str__(self):
+        return self.code
+
+    class Meta:
+        verbose_name = "Market"
+        verbose_name_plural = "Markets"
